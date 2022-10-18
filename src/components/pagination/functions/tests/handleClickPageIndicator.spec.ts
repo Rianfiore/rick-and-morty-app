244fe65b237dd6123d5ab8b045439948d9aa5142
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-10-18 12:39:53 -0300

    refactor: improve test for handle states

--- src/components/pagination/functions/tests/handleClickPageIndicator.spec.ts
+++ src/components/pagination/functions/tests/handleClickPageIndicator.spec.ts
@@ -1,17 +1,24 @@
-import { useRecoilState } from 'recoil';
+import { RecoilRoot } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { paginationState } from './../../../../recoil/pagination/atoms/index';
-import { useRecoilValue } from 'recoil';
-import { handleClickPageIndicator } from '../handleClickPageIndicator';
+import { handleClickPageIndicator, previousPage } from '../handleClickPageIndicator';
+import { renderHook } from '@testing-library/react';
 
 
 const useSetPagination = () => {
-  const [, setPagination] = useRecoilState(paginationState)
+  const [pagination, setPagination] = useRecoilState(paginationState)
 
-  return setPagination
+  return {pagination, setPagination}
 }
 
 describe("When click on Page Indicator", () => {
-  it("should call handle function", () => {
+  it("should call setState on function", () => {
+    const {result} = renderHook(() => useSetPagination(), {
+      wrapper: RecoilRoot,
+    })
+    const setPaginationState = result.current.setPagination
+    const paginationState = result.current.pagination
+
     const pagination = {
       totalPages: Array(0).fill(null),
       currentPage: 1,
@@ -19,11 +26,30 @@ describe("When click on Page Indicator", () => {
       fivePages: [1,2,3,4,5]
     }
 
-    handleClickPageIndicator('left', pagination, useSetPagination)
+    const {fivePages} = pagination
 
-    expect(handleClickPageIndicator('left', pagination, useSetPagination)).toBeCalled()
+    const mockPageIndicator = jest.fn(() => handleClickPageIndicator('right', pagination, setPaginationState))
 
+    mockPageIndicator()
+    expect(mockPageIndicator).toBeCalledTimes(1)
+    
+    const mockPreviousPage = jest.fn(() => previousPage(pagination, setPaginationState))
 
+    mockPreviousPage()
+    expect(mockPreviousPage).toBeCalledTimes(1)
 
+    const newFivePages = [
+      fivePages[0] - 1,
+      fivePages[0],
+      fivePages[1],
+      fivePages[2],
+      fivePages[3],
+    ];
+
+    const mockSetPagination = jest.fn(() => setPaginationState({...paginationState, fivePages: newFivePages}))
+
+    expect(paginationState).toStrictEqual(pagination)
+    mockSetPagination()
+    expect(mockSetPagination).toHaveBeenCalledTimes(1)
   });
 });
