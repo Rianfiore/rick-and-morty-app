659e25081f3c06fb2bb0e8b53e67124acce550d2
Rianfiore <rianfiore@gmail.com>
2022-10-18 00:06:49 -0300

    test: improve button test

--- src/components/button/index.spec.tsx
+++ src/components/button/index.spec.tsx
@@ -3,23 +3,27 @@ import { render } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "theme";
 import { hexToRGB } from "utils/functions";
+import { IButton } from "./types";
 
-const makeSut = () =>
+
+const makeSut = ({orientation = 'right', selected = false, disabled=false, children = null, icon = null} : IButton) =>
   render(
     <ThemeProvider theme={lightTheme}>
-      <Button orientation="left" />
+      <Button orientation={orientation} selected={selected} disabled={disabled} icon={icon}>{children}</Button>
     </ThemeProvider>
   );
 
 describe("Button Default Component", () => {
   it("should render", () => {
-    const { getByTestId } = makeSut();
+    const { getByTestId } = makeSut({});
+    const button = getByTestId("button")
 
-    expect(getByTestId("button")).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass('button')
   });
 
   it("should render correct styles on button", () => {
-    const { getByTestId } = makeSut();
+    const { getByTestId } = makeSut({});
 
     if (getByTestId("button")) {
       expect(getByTestId("button")).toHaveStyle({
@@ -42,31 +46,80 @@ describe("Button Default Component", () => {
     }
   });
 
-  it("should render classNames on button", () => {
-    const { getByTestId } = makeSut();
+  describe("when has conditional classNames on component", () => {
 
-    expect(getByTestId("button")).toHaveClass("button--left");
+    it('should render orientation class', () => {
+      
+    const orientation = 'left'
+    const { getByTestId } = makeSut({orientation: orientation});
+    const button = getByTestId("button")
 
-    const buttonLeft = getByTestId("button").querySelector("button--left");
-
-    if (buttonLeft) {
-      expect(buttonLeft).toHaveStyle({
+    if (orientation === 'left') {
+      expect(button).toHaveClass("button--left");
+      expect(button).toHaveStyle({
         transform: "rotate(180deg)",
       });
-    }
+    } else {
+      expect(button).not.toHaveClass("button--left");
+    }})
+
+    it('should render selected class', () => {
+      const selected = true
+      const { getByTestId } = makeSut({selected: selected});
+      const button = getByTestId("button")
+  
+      if (selected) {
+        expect(button).toHaveClass('button--selected')
+      } else {
+        expect(button).not.toHaveClass('button--selected')
+      }
+    })
+
+    it('should render disabled class', () => {
+      const disabled = true
+      const { getByTestId } = makeSut({disabled: disabled});
+      const button = getByTestId("button")
+  
+      if (disabled) {
+        expect(button).toHaveClass('button--disabled')
+      } else {
+        expect(button).not.toHaveClass('button--disabled')
+      }
+    })
+
+    it('should render size class', () => {
+      const children = 'Clique aqui'
+      const { getByTestId } = makeSut({children: children});
+      const button = getByTestId("button")
+
+      expect(button).toHaveClass('button__text--medium')
+    })
   });
 
-  it("should render correct pseudoclass hover", () => {
-    const { getByTestId } = makeSut();
-    const { getComputedStyle } = window;
+  describe('when has pseudoclass on component', () => {
+    
+    it("should render correct pseudoclass hover", () => {
 
-    window.getComputedStyle = (elt) => getComputedStyle(elt);
+      const { getByTestId } = makeSut({});
+      const { getComputedStyle } = window;
 
-    const onHover = getComputedStyle(getByTestId("button"), ":hover");
+      window.getComputedStyle = (elt) => getComputedStyle(elt);
 
-    expect(onHover).toHaveProperty(
-      "background-color",
-      hexToRGB(`${lightTheme.secondaryColor.base}`)
-    );
-  });
+      const onHover = getComputedStyle(getByTestId("button"), ":hover");
+
+      expect(onHover).toHaveProperty("background-color", hexToRGB(`${lightTheme.secondaryColor.base}`));
+    });
+  })
+
+  describe('when has conditional rendering on component', () => {
+    it ('should render button image when has icon', () => {
+      const iconPath = '/images/arrow.png'
+      const { getByTestId } = makeSut({icon: iconPath});
+      const [buttonImage] = getByTestId("button").getElementsByClassName('button__image')
+  
+      expect(buttonImage).toBeInTheDocument()
+    })
+
+
+  })
 });
